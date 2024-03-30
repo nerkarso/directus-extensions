bd5800afdb2a059c429445ccdfa99e5afffa633d
Ner Karso <nerpkarso@gmail.com>
2024-03-29 22:41:41 -0300

    refactor(current-role-hook): remove unnecessary auth refresh

--- hooks/current-role/src/index.ts
+++ hooks/current-role/src/index.ts
@@ -5,35 +5,16 @@ const hook: HookConfig = ({ embed }) => {
     'body',
     /* html */ `
 		<script>
-			const EXTENSION_NAME = '[current-role-hook]';
-
 			(async () => {
-				let accessToken;
-
 				try {
-					const authResp = await fetch(window.location.origin + '/auth/refresh', {
-						method: 'POST',
-					}).then((res) => res.json());
-					accessToken = authResp?.data.access_token;
-				} catch (error) {
-					console.error(EXTENSION_NAME, error.message);
-				}
-
-				if (!accessToken) return;
-
-				try {
-					const userResp = await fetch(window.location.origin + '/users/me?fields=role.name', {
-						headers: {
-							Authorization: 'Bearer ' + accessToken,
-						},
-					}).then((res) => res.json());
+					const userResp = await fetch(window.location.origin + '/users/me?fields=role.name').then((res) => res.json());
 					const roleName = userResp?.data?.role?.name;
 					if (roleName) {
 						const role = roleName.toLowerCase().replace(/\\s/g, '-');
 						document.body.setAttribute('data-user-role', role);
 					}
 				} catch (error) {
-					console.error(EXTENSION_NAME, error.message);
+					console.error('[current-role-hook]', error.message);
 				}
 			})();
 		</script>
