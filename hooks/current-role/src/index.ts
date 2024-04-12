b530c1b06c9c5686309d52a1eca1b19f9d7864c1
Ner Karso <nerpkarso@gmail.com>
2024-04-12 18:46:36 -0300

    feat(current-role-hook): inject user id

--- hooks/current-role/src/index.ts
+++ hooks/current-role/src/index.ts
@@ -5,14 +5,16 @@ const hook: HookConfig = ({ embed }) => {
     'body',
     /* html */ `
 		<script>
+			function injectAttribute(name, value) {
+				if (!value) return;
+				const sanitizedValue = value.toLowerCase().replace(/\\s/g, '-');
+				document.body.setAttribute(name, sanitizedValue);
+			}
 			(async () => {
 				try {
-					const userResp = await fetch(window.location.origin + '/users/me?fields=role.name').then((res) => res.json());
-					const roleName = userResp?.data?.role?.name;
-					if (roleName) {
-						const role = roleName.toLowerCase().replace(/\\s/g, '-');
-						document.body.setAttribute('data-user-role', role);
-					}
+					const response = await fetch(window.location.origin + '/users/me?fields=id,role.name').then((res) => res.json());
+					injectAttribute('data-user-id', response?.data?.id);
+					injectAttribute('data-user-role', response?.data?.role?.name);
 				} catch (error) {
 					console.error('[current-role-hook]', error.message);
 				}
