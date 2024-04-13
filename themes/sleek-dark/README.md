5a739eb2b3df68ddc56ecc3929426ed244698ce2
Ner Karso <nerpkarso@gmail.com>
2024-04-12 23:22:59 -0300

    feat(sleek-dark): init

---
+++ themes/sleek-dark/README.md
@@ -0,0 +1,15 @@
+<p align="center"><img alt="Directus Logo" src="https://user-images.githubusercontent.com/522079/158864859-0fbeae62-9d7a-4619-b35e-f8fa5f68e0c8.png"></p>
+
+# sleek-dark-theme
+
+Sleek dark theme for Directus.
+
+![Screenshot 1](https://github.com/nerkarso/directus-extensions/raw/master/themes/sleek-dark/.screenshots/01.png)
+
+## Usage
+
+1. Install the extension using a package manager or from the Marketplace:
+
+```sh
+pnpm add directus-extension-sleek-dark-theme
+```
---
+++ themes/sleek-dark/src/index.js
@@ -0,0 +1,37 @@
+import './style.css';
+
+/** @type {import('@directus/themes').Theme} */
+const theme = {
+  id: 'sleek-dark',
+  name: 'Sleek Dark',
+  appearance: 'dark',
+  rules: {
+    borderRadius: '0.5rem',
+    borderWidth: '1px',
+    form: {
+      field: {
+        input: {
+          height: '3.25rem',
+          padding: '0.25rem 0.75rem',
+        },
+      },
+      rowGap: '1.5rem',
+    },
+    navigation: {
+      background: 'transparent',
+      project: {
+        background: 'transparent',
+      },
+    },
+    sidebar: {
+      background: 'transparent',
+      section: {
+        toggle: {
+          background: 'transparent',
+        },
+      },
+    },
+  },
+};
+
+export default theme;
