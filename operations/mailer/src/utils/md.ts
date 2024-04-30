c5e32c593b575af60b554920b2bc82093927b899
Ner Karso <nerpkarso@gmail.com>
2024-04-30 20:20:54 -0300

    refactor(mailer): replace sanitize-html with DOMPurify

--- operations/mailer/src/utils/md.ts
+++ operations/mailer/src/utils/md.ts
@@ -1,11 +1,11 @@
+import DOMPurify from 'dompurify';
 import { marked } from 'marked';
-import sanitizeHTML from 'sanitize-html';
 
 /**
  * Render and sanitize a markdown string
  */
 export function md(value: string): string {
-  const markdown = marked.parse(value) as string; /* Would only be a promise if used with async extensions */
+  const html = marked.parse(value) as string; /* Would only be a promise if used with async extensions */
 
-  return sanitizeHTML(markdown);
+  return DOMPurify.sanitize(html);
 }
